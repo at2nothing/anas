f3321d9e5230861b1dcb27e1fa4ada5d26f6daab
Anas Abuelhaag <86550384+at2nothing@users.noreply.github.com>
2023-12-31 16:20:31 +0000

    Update siteProps and add smooth scrolling functionality

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Anas Abuelhaag",
+  title: "AI Engineer & Frontend Developer",
+  email: "anaselhaag@outlook.com",
+  gitHub: "at2nothing",
+  instagram: "",
+  linkedIn: "anaselhaag",
+  medium: "@anaselhaag",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/index.js
+++ src/index.js
@@ -6,5 +6,28 @@ import React from "react";
 import { render } from "react-dom";
 
 import App from "./App";
+document.addEventListener("DOMContentLoaded", function () {
+  // Select all anchor links with the hash (#) attribute
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+  // Add a click event listener to each anchor link
+  anchorLinks.forEach(link => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      // Get the target element's ID from the link's href attribute
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      // Scroll to the target element smoothly
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+});
+
 
 render(<App></App>, document.getElementById("app"));
